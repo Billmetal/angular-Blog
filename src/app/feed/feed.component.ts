ffc9e70cbcd8b5843505462f921e4b463db3e0c1
Billmetal <billmetal9@gmail.com>
2022-04-18 15:27:22 -0300

    Implementado a pesquisa por nome

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -10,11 +10,16 @@ import { PostService } from '../service/post.service';
 export class FeedComponent implements OnInit {
 
   listPosts!: Post[];
+  postsNome!: Post[];
   post: Post = new Post;
+  pesquisa!: string;
+  alertaPesquisa!: boolean;
+  alerta!: string;
 
   constructor(private postService: PostService) { }
 
   ngOnInit(): void {
+    this.alertaPesquisa = false;
     this.findPosts();
   }
 
@@ -23,10 +28,39 @@ export class FeedComponent implements OnInit {
   }
 
   publicarMensagem(){
+    this.alertaPesquisa = false;
     this.postService.postMensagem(this.post).subscribe((data: Post) => {
       this.post = data;
       location.assign("/feed");
     });
   }
+
+  pesquisarNome(){
+    this.alertaPesquisa = false;
+    if(this.listPosts.length > 0 && this.pesquisa !== ""){
+      this.postsNome = [];
+      this.listPosts.forEach(value => {
+        if(value.nome.includes(this.pesquisa)){
+          this.postsNome.push(value);
+        }
+      });
+      if(this.postsNome.length > 0){
+        this.listPosts = this.postsNome;
+      } else {
+        
+        this.alerta = "Nenhum comentário encontrado com esse nome ."
+        this.alertaPesquisa = true;
+      }
+    } else {
+      if(this.pesquisa === ""){
+        location.assign("/feed");
+      } else {
+        this.alerta = "Não há comentários para buscar ."
+        this.alertaPesquisa = true;
+      }
+    }
+  }
+
+  // Nenhum comentário encontrado com esse nome .
 
 }
